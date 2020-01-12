82eaf654cca14055e5e95891570a848827ada898
Alex Couture-Beil <alex@mofo.ca>
2020-01-12 10:19:06 -0800

    hold logs in memory

--- tinysyslogd.py
+++ tinysyslogd.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python3
 
-LISTEN_HOST = "0.0.0.0"
-LISTEN_PORT = 514
+SYSLOG_LISTEN_HOST = "0.0.0.0"
+SYSLOG_LISTEN_PORT = 514
+
+DUMP_LISTEN_HOST = "0.0.0.0"
+DUMP_LISTEN_PORT = 8098
+
+MAX_LOGS = 10000
 
 import socketserver
+import threading
+import time
+
+lock = threading.Lock()
+logs = []
 
 class SyslogHandler(socketserver.BaseRequestHandler):
-
     def handle(self):
         client = str(self.client_address)
         msg = str(self.request[0].strip())
-        print(f'{client}: {msg}')
+        logs.append(f'{client}: {msg}')
+        with lock:
+            logs = logs[-MAX_LOGS:]
+
+class DumpHandler(socketserver.StreamRequestHandler):
+    def handle(self):
+        self.wfile.write('logs\n'.encode('utf8'))
+        with lock:
+            for l in logs:
+                self.wfile.write((l + '\n').encode('utf8'))
 
 if __name__ == "__main__":
-    server = socketserver.UDPServer((LISTEN_HOST,LISTEN_PORT), SyslogHandler)
-    server.serve_forever()
+    syslog_server = socketserver.UDPServer((SYSLOG_LISTEN_HOST, SYSLOG_LISTEN_PORT), SyslogHandler)
+    dump_server = socketserver.TCPServer((DUMP_LISTEN_HOST, DUMP_LISTEN_PORT), DumpHandler)
+    dump_server.allow_reuse_address = True
+
+    threads = []
+    for server in (syslog_server, dump_server):
+        thread = threading.Thread(target=server.serve_forever)
+        thread.start()
+        threads.append(thread)
+
+    try:
+        while 1:
+            time.sleep(1)
+    except:
+        print('shutting down')
+        for server in (syslog_server, dump_server):
+            server.shutdown()
+        for thread in threads:
+            thread.join()
+
